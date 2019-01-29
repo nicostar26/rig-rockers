688948bb2b7e98e674e2ef0f974eb79bb08d913a
Nicole Saunders <nicostar26@yahoo.com>
2019-01-29 12:45:35 -0500

    details 1-29-19

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
         </div>
     <!-- END OF SLIDER -->
 
-
+<!-- ABOUT WEBSITE -->
 <div class="container">
     <div class="about">
         <div class="soldierrv">
@@ -49,27 +49,39 @@
     </div>
 </div>
 
+<!-- ROBERT HAMA -->
 <div class="container">
+    <h2 id="hama">Meet Robert Hama</h2>
     <div class="hama" id="hama">
+    
         <div class="hama-content">
-            <h2>Meet Robert Hama</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum modi qui sequi quae quod consequatur, at praesentium deleniti cum saepe. Incidunt officiis odio, nobis magnam consequatur numquam ex, saepe necessitatibus, aperiam dolore ipsum magni debitis ut? Harum, error nemo dignissimos, fuga, nulla hic quaerat culpa quasi tenetur molestias nihil ducimus maxime. Molestias soluta, modi minus fuga illo rerum quam perferendis odio quasi aliquam adipisci, sint neque possimus sunt omnis facere placeat, quo distinctio! Reiciendis, rerum.</p>
+            
+            <p>Robert Hama represents the vulnerable emotional inner core that most of us have, most particularly, veterans that have experienced the absurd experience and environment of serving and having the impact that combat brings to any person. Whether they’re a soldier or a civilian, Robert speaks to that experience.</p>
+
+            <p>Robert allows individuals to really express themselves in a safe way. No different than individuals who make the decision to abuse substances and elude themselves from the violence of this world. That’s what Robert speaks to.}</p> 
+
+            <p>He allows me to experience that with humor. He allows me to experience that in open truth using humor as a vehicle and that brings healing. Robert allowed me to laugh… and I mean laugh in a genuine way. Not in a cynical jaded way, but in a very open way.</p> 
+
+            <p>Humor has always been a beautiful vehicle for healing, and it truly is. And by the way… it’s fun. 
+            </p>
         </div>
         <div class="hamapic">
-            <img src="img/hamapic.png" alt="Robert Hama">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/0c1FWF3kBmo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
         
     </div>
 </div>
 
-
+<!-- VETERAN INFO LINK -->
 <div class="container">
 <div class="vetinfo">
-    <h1>Are you a veteran?</h1>
-    <button class="vetinfobtn">Click Here</button>
+    <h1 id="vetinfo">Are you a veteran?</h1>
+    <button class="vetinfobtn"><a href="vetinfo.php">Helpful Info</a></button>
 </div>
 </div>
 
+
+<!-- ABOUT MARK AND FAITH -->
 <div class="container">
         <div class="aboutus">
                 <div class="edgren1">
@@ -86,19 +98,22 @@
         </div>
     </div>
 
+
+<!-- YOUTUBE VIDEOS -->
 <div class="container">
     <div class="videos">
 
         <h2>Videos We Like</h2>
 
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/9YffrCViTVk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/Xp1hWord_hg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/9YffrCViTVk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/nFGZffjMe2o" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/9YffrCViTVk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/IkL4lVMJqDk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
     </div>
 
+<!-- PODCASTS -->
     <div class="container">
     <div class="podcasts">
         <h2>Podcasts We Like</h2>
@@ -112,6 +127,7 @@
     </div>
     </div>
 
+<!-- AFFILIATES -->
 <div class="container">
     <div class="affiliates">
         <h2>Affiliates</h2>
@@ -119,8 +135,12 @@
     </div>
 </div>
 
+
+<!-- CONTACT FORM -->
 <?php include 'inc/contactform.php'; ?>
 
+
+<!-- FOOTER -->
 <?php include 'inc/footer.php'; ?>
 
 <script src="js/scriptslider.js"></script>
--- vetinfo.php
+++ vetinfo.php
@@ -1,6 +1,7 @@
 <?php include 'inc/header.php'; ?>
 <div class="container">
 <h1>Veteran Information Will Go Here</h1>
+<a href="http://invisiblewound.com">Invisible Wound (see if you can get thier logo, too</a>
 </div>
 <?php include 'inc/footer.php'; ?>
 
