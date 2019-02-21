eb446184b8d5812b091c9416ea9ba8e55267af8f
Nicole Saunders <nicostar26@yahoo.com>
2019-02-21 13:08:19 -0500

    updates

--- rvinfo.php
+++ rvinfo.php
@@ -9,37 +9,37 @@
     </div>
     <div class="container">
 
+         <div class="pastrigs-title">
+            <h2>Blast From The Past!</h2>
+            <p>A look from our first rig to our current! (left to right)</p>
+        </div>
+
     <div class="pastrigs">
+       
         
         <div class="rig1">
             <img src="img/rig1.jpg" alt="Rig 1">
-            <h3>First Rig</h3>
-            <p>Lorem ipsum dolor sit amet.</p>
+            
         </div>
         <div class="rig2">
             <img src="img/rig2.jpg" alt="Rig 2">
-            <h3>First Rig</h3>
-            <p>Lorem ipsum dolor sit amet.</p>
+            
         </div>
         <div class="rig3">
             <img src="img/rig3.jpg" alt="Rig 3">
-            <h3>First Rig</h3>
-            <p>Lorem ipsum dolor sit amet.</p>
+            
         </div>
         <div class="rig4">
             <img src="img/rig4.jpg" alt="Rig 4">
-            <h3>First Rig</h3>
-            <p>Lorem ipsum dolor sit amet.</p>
+            
         </div>
         <div class="rig5">
             <img src="img/rig5.jpg" alt="Rig 5">
-            <h3>First Rig</h3>
-            <p>Lorem ipsum dolor sit amet.</p>
+            
         </div>
         <div class="rig6">
             <img src="img/rig6.jpg" alt="Rig 6">
-            <h3>First Rig</h3>
-            <p>Lorem ipsum dolor sit amet.</p>
+            
         </div>
     </div>
     </div>
