101af8380601176c04f17418e320fc5a9ae7a1c7
Nicole Saunders <nicostar26@yahoo.com>
2019-03-31 09:01:05 -0400

    updated rv page

--- rvinfo.php
+++ rvinfo.php
@@ -52,5 +52,32 @@
     </div>
     </div>
     
+    <div class="container">
+<div class="rvlinks">
+<h2>Some Helpful Resources</h2>
+
+<h4>RV Repairs</h4>
+
+
+<a href="https://m.youtube.com/watch?v=8XeROaBV4t8&feature=share">Repairing a delaminated RV wall and bubbled Fiberglass</a>
+<a href="https://m.youtube.com/watch?feature=share&v=nmzl9x5zdLY">DIY RV Repairs</a>
+
+<h4>Showroom</h4>
+<a href="https://m.youtube.com/watch?feature=share&v=PZUocsMsNFU">Grand Design Momentum 380TH Walkthrough</a>
+<a href="https://www.granddesignrv.com/">Grand Design RV</a>
+
+<h4>RV and other useful Websites</h4>
+<a href="https://m.winnebagoind.com/">Winnebago Ind</a>
+<a href="https://www.goodsam.com/club/defaultb.aspx?utm_expid=.VwdKy4pcS9K-2d_A_vhVZA.1&utm_referrer=https%3A%2F%2Fwww.google.com%2F">GoodSam</a>
+<a href="https://www.zoro.com/tents-and-camping-equipment">Zoro</a>
+<a href="https://www.usaa.com/inet/pages/profile_and_preferences?akredirect=true">USAA</a>
+<a href="https://www.aaa.com/?rclub=111&rurl=https%3A//carolinas.aaa.com/">AAA</a>
+<a href="https://www.ammoland.com/2014/03/traveling-cross-country-with-a-gun-in-your-rv/#axzz5jCvEGqmG">Ammoland</a>
+<a href="https://www.gasbuddy.com/">Gasbuddy</a>
+
+
+
+</div>
+</div>
 
 <?php include 'inc/footer.php'; ?>
